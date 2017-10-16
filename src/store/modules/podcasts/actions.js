c546fac7bb1f3012eebc795accee00da17465cbd
Nico Audy <nicoaudy1994@gmail.com>
2017-10-16 15:39:51 +0700

    change actions and mutations for load more

--- src/store/modules/podcasts/actions.js
+++ src/store/modules/podcasts/actions.js
@@ -3,5 +3,9 @@ import * as api from '@/api/podcasts'
 export const getPodcasts = ({ commit }, page = 1) => {
   api.getPodcasts(page).then((response) => {
     commit('setPodcasts', response.data.data)
+    commit('setPage', {
+      current: page,
+      max: response.data.meta.pagination.total_pages
+    })
   })
 }
--- src/store/modules/podcasts/mutations.js
+++ src/store/modules/podcasts/mutations.js
@@ -1,3 +1,8 @@
 export const setPodcasts = (state, podcasts) => {
   state.podcasts = podcasts
 }
+
+export const setPage = (state, { current, max }) => {
+  state.page.current = current
+  state.page.max = max || state.page.max
+}

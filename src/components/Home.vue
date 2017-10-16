f375e3bb59e918323e66aa95a1fdcbf2989d3a5b
Nico Audy <nicoaudy1994@gmail.com>
2017-10-16 15:41:27 +0700

    if has more page show button else hide

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    {{ page }}
     <transition-group name="fade">
       <podcast :key="podcast.id" v-for="podcast in podcasts" :podcast="podcast"></podcast>
     </transition-group>
-    <a href="#" class="load-more">Load older podcasts</a>
+    <a href="#" class="load-more" v-if="page.hasMore()">Load older podcasts</a>
   </div>
 </template>
 
